3d42049bb9a28c1c85ca1d9d5822fd0d0ca80f60
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-11-15 00:22:34 +0800

    email

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -41,7 +41,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider {
     protected function gate() {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'admin@admin.com'
             ]);
         });
     }
