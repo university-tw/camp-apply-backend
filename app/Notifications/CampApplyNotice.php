757ece57a813314805ecbdf3ef59ad8b7e3767e2
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-11-15 01:23:22 +0800

    notification

--- app/Notifications/CampApplyNotice.php
+++ app/Notifications/CampApplyNotice.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Models\Apply;
 use App\Models\Camp;
 use App\Models\Group;
 use Illuminate\Bus\Queueable;
@@ -11,17 +12,15 @@ use Illuminate\Notifications\Notification;
 class CampApplyNotice extends Notification {
     use Queueable;
 
-    public Camp $camp;
-    public Group $group;
+    public Apply $apply;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct(Camp $camp, Group $group = null) {
-        $this->camp = $camp;
-        $this->group = $group;
+    public function __construct(Apply $apply) {
+        $this->apply = $apply;
     }
 
     /**
@@ -43,13 +42,15 @@ class CampApplyNotice extends Notification {
     public function toMail($notifiable) {
         $mail = (new MailMessage)
             ->subject('營隊報名結果')
-            ->line($this->camp->name . '報名成功');
+            ->line($this->apply->camp_time->camp->name . '報名成功');
 
-        if ($this->group) {
-            $mail->line('您的隊伍名稱為：' . $this->group->name)->line('您的隊伍ID為：' . $this->group->id);
+        if ($this->apply->offer->group) {
+            $mail->line('您的隊伍名稱為：' . $this->apply->group->name)->line('您的隊伍ID為：' . $this->apply->group->id);
         }
 
         $mail->line("請儘速依照您提供的帳戶資訊轉帳")
+            ->line("請使用： (" . $this->apply->bank_code . ")" . $this->apply->bank_account . " 轉帳")
+            ->line("繳費金額： " . $this->apply->offer->price)
             ->line('感謝您使用營隊報名平台');
 
         return $mail;
