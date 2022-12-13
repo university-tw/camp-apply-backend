2865399f7f596e21007d163d62f4f72fd359574f
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-12-13 14:34:43 +0800

    Fix price

--- database/seeders/UniversityTWCamps2023.php
+++ database/seeders/UniversityTWCamps2023.php
@@ -5,13 +5,15 @@ namespace Database\Seeders;
 use App\Models\Camp;
 use Illuminate\Database\Seeder;
 
-class UniversityTWCamps2023 extends Seeder {
+class UniversityTWCamps2023 extends Seeder
+{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run() {
+    public function run()
+    {
         $camp = Camp::create([
             'name' => '2023 擊劍體驗冬令營',
             'description' => '2023 擊劍體驗冬令營',
@@ -49,8 +51,8 @@ class UniversityTWCamps2023 extends Seeder {
         $camp->times()->createMany([
             [
                 'name' => '第一梯次',
-                'start' => '2023-02-06',
-                'end' => '2023-02-08',
+                'start' => '2023-02-02',
+                'end' => '2023-02-04',
             ]
         ]);
         $camp->offers()->createMany([
@@ -62,13 +64,49 @@ class UniversityTWCamps2023 extends Seeder {
             [
                 'name' => '超早鳥優惠',
                 'price' => 5000,
-                'priceValidUntil' => '2022-12-15 23:59:59',
+                'priceValidUntil' => '2022-12-20 23:59:59',
                 'group' => false,
             ],
             [
                 'name' => '限定優惠',
                 'price' => 3000,
                 'limit' => 10,
+                'group' => false,
+            ],
+            [
+                'name' => '團報優惠',
+                'price' => 4000,
+                'group' => true,
+            ],
+        ]);
+        $camp->bank_accounts()->attach(1);
+        $camp = Camp::create([
+            'name' => '2023 Python實作營',
+            'description' => '2023 Python實作營',
+        ]);
+        $camp->times()->createMany([
+            [
+                'name' => '第一梯次',
+                'start' => '2023-02-06',
+                'end' => '2023-02-08',
+            ]
+        ]);
+        $camp->offers()->createMany([
+            [
+                'name' => '清寒專案',
+                'price' => 600,
+                'limit' => 10,
+                'group' => false,
+            ],
+            [
+                'name' => '一般報名',
+                'price' => 8000,
+                'group' => false,
+            ],
+            [
+                'name' => '超早鳥優惠',
+                'price' => 3999,
+                'priceValidUntil' => '2022-12-20 23:59:59',
                 'group' => false,
             ],
             [
